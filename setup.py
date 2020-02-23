8999930237aa08fb5a4ba2a11a4ef80a3534c344
gammaflauge <mddevine@gmail.com>
2020-02-22 20:35:24 -0500

    :tada: Initial commit from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='machine learning on the wine dataset',
+    author='gammaflauge',
+    license='MIT',
+)
